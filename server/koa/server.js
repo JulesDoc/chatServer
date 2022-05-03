8b09f4255d63500b085d1bf79ec1240741e47287
Calvo <julio.calvo@itu.int>
2022-05-03 16:22:36 +0200

    Another way of doing

--- server/koa/server.js
+++ server/koa/server.js
@@ -1,4 +1,6 @@
 /* eslint-disable linebreak-style */
+/* /* eslint-disable linebreak-style */
+/*
 const http = require('http');
 const koa = require('koa');
 const bodyParser = require('koa-bodyparser');
@@ -15,7 +17,28 @@ app.use(ctx => {
 });
 
 app.listen(3000);
-console.log('Running!!');
+console.log('Running!!'); */
+
+// Modules
+const bodyParser = require('koa-bodyparser');
+const Koa = require('koa');
+const path = require('path');
+const serve = require('koa-static');
+const router = require('C:/Users/calvo/Desktop/WebTraining/7 - ChatServer/chat-server-exercise/routes/homeRouter.js');
+
+
+// Expected here; serve static files from public dir
+const staticDirPath = path.join(__dirname, '../../client');
+// Init Koa.js server
+const server = new Koa();
+// Mount the middleware
+server.use(serve(staticDirPath))
+  .use(bodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods());
+// Run Koa.js server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server Listening on PORT ${PORT} 🚀 ..`));
 
 /* const hostname = '127.0.0.1';
 const db = {msgs: []};
